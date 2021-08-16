fa91bcf6e258bc64bb6fc5d652d768c1e1e2dca8
anushri2122009 <76483722+anushri2122009@users.noreply.github.com>
2021-08-16 17:14:45 +0530

    Add files via upload

---
+++ screens/BookT_Screen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class BookT_Screen extends React.Component {
+    render() {
+        return(
+            <View style = {{justifyContent: 'center', alignItems:'center', margin:'25%' , alignSelf:"center"}}>
+                <Text>Issue or Return</Text>
+            </View>
+        )
+    }
+}
